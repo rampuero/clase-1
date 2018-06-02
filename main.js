bd3046dc9294b485398f42842697255e08062b06
Ricardo Ampuero <ampuero.ar@gmail.com>
2018-06-01 22:50:29 -0400

    Este es mi primer ejercicio

---
+++ main.js
@@ -0,0 +1,132 @@
+// JavaScript Document
+
+/*var int = 1;
+var str = "hola mundo";
+var und;
+var boll = true;
+
+console.log(typeof int);
+console.log(typeof str);
+console.log(typeof und);
+console.log(typeof boll);
+
+var number1 = 10;
+
+var number2 = 20;
+
+console.log("el resultado de la suma es:", number1 + number2 + " dvsfvsfvfs");
+console.log("el resultado de la resta es:", number1 - number2);
+console.log("el resultado de la multiplicación es:", number1 * number2);
+console.log("el resultado de la división es:", number1 / number2);
+
+/*var number = 1;
+
+console.log(number);
+var number2 = number++;
+
+console.log(number2);
+
+var bool1 = true;
+var bool2 = false;
+
+console.log(bool1 === bool2);
+console.log(bool1 !== bool2);
+console.log(bool1 !== bool2);
+console.log(bool2 === bool2);
+
+var number1 = 10;
+
+var number2 = 20;
+
+console.log(number1 < number2);
+console.log(number1 > number2);
+console.log(number1 <= number2);
+console.log(number1 >= number2);
+
+var number1 = 10;
+var number2 = 20;
+
+
+console.log(number1 < number2 && number2 > number1 );
+console.log(number1 < number2 && number2 >= number1 );
+console.log(number1 > number2 && number2 > number1 );
+console.log(number1 > number2 && number2 < number1 );
+
+var number1 = 10;
+var number2 = 20;
+
+
+console.log(number1 < number2 || number2 > number1 );
+console.log(number1 < number2 || number2 >= number1 );
+console.log(number1 > number2 || number2 > number1 );
+console.log(number1 > number2 || number2 < number1 );
+
+var number1 = 10;
+var number2 = 20;
+
+
+//console.log(number1 < number2 ? 'si es verdadero': 'no es falso');
+//console.log(number1 > number2 ? 'si es verdadero': 'no es falso');
+
+if(number1 < number2 && number1 > number2 ){
+	console.log("se ejecuta")
+	}
+	else {
+		console.log("no se ejecuta");
+	}
+	
+var number1 = 10;
+var number2 = 20;
+
+function rango(number1){
+		if(number1 <= 5){
+			return 0;
+		}else if(7 >= 6 && 7 <= 10){
+			return 1
+		}
+	}
+	
+switch(number1){
+	case 0:
+		console.log("es igual a 10");		
+		break;
+	case 1:
+		console.log("es igual a 20");
+		break;
+	case 30:
+		console.log("es igual a 30");
+		break;
+	default:
+		console.log("es otro valor");			
+}
+
+function nombreCompleto (nombre,apellido){
+	// bloque de codigo
+	console.log(nombre + " " + apellido);
+}
+
+nombreCompleto("ricardo","Ampuero");	
+
+function edad(anioNac){
+	return 2018 - anioNac
+}
+
+console.log(edad(1989));*/
+
+var edad = prompt("cual es su edad");
+
+function anio (){
+	if(edad > 18){
+		console.log("usted es mayor de edad");
+	}
+	else {
+		console.log("usted es menor de edad");
+	}	
+}
+
+anio();
+
+
+
+
+
